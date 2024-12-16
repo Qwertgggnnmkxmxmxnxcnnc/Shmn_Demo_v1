a74de4215472a39bce575ff9be036787e87ee5d5
codeded <egorlevadniy@mail.ru>
2024-12-16 21:10:56 +0300

    Ready for deployment

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import { CartProvider } from './components/cart/CartContext';
 import { QuickOrderProvider } from './components/quickOrder/QuickOrderContext';
 import CartPanel from './components/cart/CartPanel';
